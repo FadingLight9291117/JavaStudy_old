ac34ba735063f0197dd346a5aa0bb8702757da0e
clz <fadinglight9291117@126.com>
2019-06-17 23:53:00 +0800

    SimpleDateFormat

--- Java常用类/格式化类/SimpleDataTest.java
+++ Java常用类/格式化类/SimpleDataTest.java
@@ -1,9 +1,30 @@
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 /**
- * SimpleDataTest
+ * SimpleDataFormat 工厂模式 parse格式化字符串为时间对象 format将时间格式化为字符串
  */
 public class SimpleDataTest {
 
     public static void main(String[] args) {
-    	System.out.println("Hello World");
+
+        Calendar cal = Calendar.getInstance();
+        Date now = cal.getTime();
+        System.out.println(now);
+        SimpleDateFormat format = new SimpleDateFormat("yyyy年MM月dd日");
+        System.out.println(format.format(now));
+
+        // 从字符串提取出日期
+        String strDate = "2008-11-17";
+        String pat = "yyyy-MM-dd";
+        SimpleDateFormat format2 = new SimpleDateFormat(pat);
+        Date d = null;
+        try {
+            d = format2.parse(strDate);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println(format.format(d));
     }
 }
